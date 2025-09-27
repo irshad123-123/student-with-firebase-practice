85f09391d50b6981a9f639cbdb873a959aa4edcb
irshad123-123 <shaikhirshadwahab@gmail.com>
2025-09-27 16:28:36 +0530

    spinner added

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+let cl = console.log;
+
+const studentForm = document.getElementById('studentForm')
+const fname = document.getElementById('fname')
+const lname = document.getElementById('lname')
+const email = document.getElementById('email')
+const contact = document.getElementById('contact')
+const addStudent = document.getElementById('addStudent')
+const updateStudent = document.getElementById('updateStudent')
+const studentContainer = document.getElementById('studentContainer')
+const loader = document.getElementById('loader')
